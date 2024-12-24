8b9ccabe4b0c9ac780486f925629439a91195494
Maitry Makwana <maitrymakwana196@gmail.com>
2024-12-25 00:49:11 +0530

    Initialized project

---
+++ src/main/java/org/finsible/backend/FinsibleApplication.java
@@ -0,0 +1,13 @@
+package org.finsible.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinsibleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinsibleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/finsible/backend/FinsibleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.finsible.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinsibleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
