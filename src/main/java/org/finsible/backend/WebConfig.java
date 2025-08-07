e19974de6f85e647422c8a3f4a023dc134c5e15a
Maitry Makwana <maitrymakwana196@gmail.com>
2025-08-08 00:39:29 +0530

    Added CORS configuration to allow requests from specified origins

---
+++ src/main/java/org/finsible/backend/WebConfig.java
@@ -0,0 +1,21 @@
+package org.finsible.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    // rejects the request before it reaches the controller if the request does not match the allowed origins, methods, and headers.
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // for cookies
+    }
+}
