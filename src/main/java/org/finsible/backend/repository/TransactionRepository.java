f23fb4dc4a0ca90b6fbbf546f3752d4ce92994cd
Jeel Patel <itsjeel01@gmail.com>
2026-01-25 00:37:08 +0530

    Remove deprecated transaction fetching methods from repository

--- src/main/java/org/finsible/backend/repository/TransactionRepository.java
+++ src/main/java/org/finsible/backend/repository/TransactionRepository.java
@@ -7,27 +7,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import java.util.List;
 import java.util.Optional;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     Optional<Transaction> findByIdAndCreatedBy(Long id, String createdBy);
-
-    List<Transaction> findAllByTypeAndCreatedBy(Type type, String userId);
-
-    List<Transaction> findAllByCategory_IdAndCreatedBy(Long categoryId, String userId);
-
-    List<Transaction> findAllByToAccount_IdAndCreatedBy(Long accountId, String userId);
-
-    List<Transaction> findAllByFromAccount_IdAndCreatedBy(Long accountId, String userId);
-
-    List<Transaction> findAllByToAccount_AccountGroup_IdAndCreatedBy(Long toAccountAccountGroupId, String createdBy);
-
-    List<Transaction> findAllByFromAccount_AccountGroup_IdAndCreatedBy(Long accountGroupId, String userId);
-
-    List<Transaction> findAllByToAccount_IdOrFromAccount_IdAndCreatedBy(Long toAccount_id, Long fromAccount_id, String createdBy);
-
-    List<Transaction> findAllByToAccount_AccountGroup_IdOrFromAccount_AccountGroup_IdAndCreatedBy(Long toAccount_accountGroup_id, Long fromAccount_accountGroup_id, String createdBy);
 
     @Query("SELECT t FROM Transaction t " +
            "LEFT JOIN t.toAccount toAcc " +
